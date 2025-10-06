a4be6676739fad92a5faf9c100937e7448d159b2
Ramkumar <thalar616@gmail.com>
2025-10-06 09:45:25 +0530

    Updated backend API link to Render deployment

--- server.js
+++ server.js
@@ -9,7 +9,15 @@ Connection();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({
+  origin: ["https://portfolio-frontend-34i4.onrender.com",
+           "https://portfolio-frontend-34i4.onrender.com/contact",
+           "http://localhost:3134"
+          ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+}));
+
 
 app.use('/api/v1', V1);
 
