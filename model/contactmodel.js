2d616139454232bd995afb1f1b4ccd7aaf36c598
Ramkumar <thalar616@gmail.com>
2025-10-08 19:09:28 +0530

    Updated backend API link to Render deployment

--- model/contactmodel.js
+++ model/contactmodel.js
@@ -1,64 +1,34 @@
 const mongoose = require('mongoose');
 
 const contactSchema = new mongoose.Schema({
-  name: { 
-    type: String, 
-    required: true 
-  },
-  email: { 
-    type: String, 
-    required: true 
-  },
-  brief: { 
-    type: String, 
-    required: true 
-  },
-  websiteUrl: { 
-    type: String 
-  },
-  companyStage: {
-    type: String,
-    required: false,
-    enum: [
-      'startup', 
-      'established', 
-      'individual', 
-      ''
-    ],
-  },
-  deadline: {
-    type: String,
-    required: false,
-    enum: [
-      'urgent', 
-      'flexible', 
-      'no-deadline', 
-      ''
-    ],
-  },
-  budget: {
-    type: String,
-    required: false,
-    enum: [
-      '5k-10k', 
-      '10k-20k', 
-      '20k-plus', 
-      ''
-    ],
-  },
-  howHeard: {
-    type: [String],
-    required: false,
-    enum: [
-      'Webflow partners',
-      'Awwwards',
-      'Search engine',
-      'Social media',
-      'Word of mouth',
-      'News article or blog',
-      'Other',
-    ],
-  },
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  brief: { type: String, required: true },
+  websiteUrl: { type: String },
+  companyStage: {
+    type: String,
+    enum: ['startup', 'established', 'individual', null],
+  },
+  deadline: {
+    type: String,
+    enum: ['urgent', 'flexible', 'no-deadline', null],
+  },
+  budget: {
+    type: String,
+    enum: ['5k-10k', '10k-20k', '20k-plus', null],
+  },
+  howHeard: {
+    type: [String],
+    enum: [
+      'Webflow partners',
+      'Awwwards',
+      'Search engine',
+      'Social media',
+      'Word of mouth',
+      'News article or blog',
+      'Other',
+    ],
+  },
 });
 
 const Contact = mongoose.model('Contact', contactSchema);
