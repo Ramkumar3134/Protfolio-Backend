46f6ccdd4761a3ba90b70e17762ee71419ddbb2b
Ramkumar <thalar616@gmail.com>
2025-10-08 15:50:03 +0530

    Fixed CORS origin and API URL

--- server.js
+++ server.js
@@ -10,9 +10,7 @@ Connection();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: ["https://portfolio-frontend-34i4.onrender.com",
-           "https://portfolio-frontend-34i4.onrender.com/contact"
-          ],
+  origin: "https://portfolio-frontend-34i4.onrender.com",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 }));
