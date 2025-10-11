7339a32689b64a6e50e9a61f45d4c6a975df0066
Ramkumar <thalar616@gmail.com>
2025-10-11 13:56:18 +0530

    Updated backend API link to Render deployment

--- route/contactRoute.js
+++ route/contactRoute.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const {createContect} = require('../controller/contactController')
-const middleCheck = require('../middleware/contactmiddle')
 const router = express.Router();
 
 router.post('/createContact', createContect)
